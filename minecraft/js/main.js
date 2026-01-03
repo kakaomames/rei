344eb4f8420bb4b973244c35a6aba2ec18bcb916
kakaomame <nattyannkunndesu@icloud.com>
2026-01-03 09:59:18 +0900

    Update main.js

--- minecraft/js/main.js
+++ minecraft/js/main.js
@@ -1,13 +1,23 @@
 // 【main.js】
-RenderBridge.init();
+async function startGravityTest() {
+    RenderBridge.init();
 
-// 1. assets/textures/にあるファイル名を指定するだけ！
-const grassMaterial = RenderBridge.createBlockMaterial('grass_side.png');
+    // 1. 地面を設置 (grass block)
+    const grassData = await AddonManager.loadBlock('grass'); 
+    grassData.geometry.forEach(cube => RenderBridge.createMesh(cube, grassMaterial));
 
-// 2. GeometryCoreで解析済みのデータを使う
-const cubes = GeometryCore.parse(sampleJson);
+    // 2. スティーブを読み込んで召喚
+    const response = await fetch('RP/models/entity/steve.geometry.json');
+    const steveRaw = await response.json();
+    const steveCubes = GeometryCore.parse(JSON.stringify(steveRaw));
+    
+    PlayerPhysics.init(steveCubes, steveSkinMaterial);
 
-// 3. 召喚！
-cubes.forEach(cube => {
-    RenderBridge.createMesh(cube, grassMaterial);
-});
+    // 3. ゲームループ（毎フレーム実行）
+    function loop() {
+        // 地面データの配列を渡して物理演算！
+        PlayerPhysics.update(grassData.geometry); 
+        requestAnimationFrame(loop);
+    }
+    loop();
+}
