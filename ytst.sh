2e76bacdcd1644bb21f5c4e888cb736a4c909355
kakaomame <nattyannkunndesu@icloud.com>
2026-09-05 10:53:16 +0900

    Update ytst.sh

--- ytst.sh
+++ ytst.sh
@@ -5,6 +5,9 @@ if [ "$1" = "u" ]; then
     echo "--- スクリプトを最新版にアップデート中... ---"
     curl -L "https://raw.githubusercontent.com/kakaomames/rei/refs/heads/main/ytst.sh" -o ytst.sh
     curl -L "https://raw.githubusercontent.com/kakaomames/rei/refs/heads/main/main.v4.py" -o main.py
+    curl -L "https://github.com/kakaomames/rei/archive/refs/heads/main.zip" -o main.zip
+    unzip main.zip ytst.sh
+    unzip main.zip main.py
     echo "✨ アップデートが完了しました！"
     echo "動画のURLを指定してもう一度実行してください（例: ./ytst.sh URL）"
     exit 0 # アップデート時はここで処理を終了する
@@ -21,4 +24,4 @@ if [ ! -f "main.py" ]; then
 fi
 
 # 💡ボット検知対策：curl -c で上書きせず、手動で置いた cookies.txt をそのままPythonに読ませる
-python main.py "$1"
+python main.py "$1" 
